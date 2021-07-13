aa95e22ffe0a6df4c6354c25bbfcf5438c0a483b
Mcube728 <boredAstronaut728@protonmail.com>
2021-07-13 09:29:56 +0530

    Changed colour of the window borders

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_blue[]        = "#1241aa";
+static const char col_blue[]        = "#0340c1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
