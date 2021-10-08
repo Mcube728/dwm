89d635758c45f93e9a026be0172da03d5bcafd64
Mcube728 <83777213+Mcube728@users.noreply.github.com>
2021-10-08 10:06:43 +0530

    changed a colour

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_ac[]    	    = "#0340c1";
+static const char col_ac[]    	    = "#224488";
 static const char col_bg[]          = "#000000";
 static const char col_bor[] 	    = "#FFFFFF";
 
